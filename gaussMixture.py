8e89df9360f76682b23d21e367bdd1f06a4ed495
Den Raskovalov <denplusplus@fb.com>
2016-09-20 21:39:31 -0700

    better initial

--- gaussMixture.py
+++ gaussMixture.py
@@ -35,6 +35,7 @@ def getRegion(name):
 with open("table_233_level_4.txt") as fIn:
     tsvIn = csv.reader(fIn, delimiter='\t')
     header = None
+    someArea = None
     for index, row in enumerate(tsvIn):
         if 0 != index:
             area = Area()
@@ -50,6 +51,7 @@ with open("table_233_level_4.txt") as fIn:
             area._uik = getRegion(row[0] + row[1] + row[2] + row[3])
             area.normalize()
             areas.append(area)
+            someArea = area
         else:
             header = row[:]
             print(header)
@@ -59,13 +61,19 @@ with open("table_233_level_4.txt") as fIn:
 print("Areas = %d" % len(areas))
 print("Regions = %d" % len(region2id))
 
-numpyTarget = numpy.random.uniform(-1.0, 1.0, (len(areas), N_PARTIES))
+numpyTarget = numpy.random.uniform(-1.0, 1.0, (len(areas), N_PARTIES)).astype(theano.config.floatX)
 for i, area in enumerate(areas):
     for j in range(N_PARTIES):
         numpyTarget[i][j] = area._votes[j]
-target = theano.shared(name='target', value=numpyTarget).astype(theano.config.floatX)
-embeddings = theano.shared(name='embeddings', value=0.2 * numpy.random.uniform(0.01, 0.5, (len(region2id), N_PARTIES)).astype(theano.config.floatX))
-mixture = theano.shared(name='mixture', value=0.2 * numpy.random.uniform(0.1, 0.2, (len(areas), 5)).astype(theano.config.floatX))
+target = theano.shared(name='target', value=numpyTarget)
+numpyEmbeddings = numpy.random.uniform(-1.0, 1.0, (len(region2id), N_PARTIES)).astype(theano.config.floatX)
+for i, area in enumerate(region2id):
+    numpyEmbeddings = someArea._votes[:]
+embeddings = theano.shared(name='embeddings', value=numpyEmbeddings)
+numpyMixture = numpy.random.uniform(0.1, 0.2, (len(areas), 5)).astype(theano.config.floatX)
+for i in range(len(areas)):
+    numpyMixture[i][:] = [0, 0, 0, 0, 1]
+mixture = theano.shared(name='mixture', value=numpyMixture)
 numpyIndices = numpy.zeros( shape=(len(areas), 5), dtype='int32' )
 for i, area in enumerate(areas):
     numpyIndices[i] = [area._russia, area._region, area._oik, area._tik, area._uik]
@@ -73,12 +81,15 @@ indices = theano.shared(numpyIndices)
 areasIndices = T.arange(len(areas))
 params = [embeddings, mixture]
 
-loss = theano.scan( lambda i: (target[i] - mixture[i][0]*embeddings[indices[i][0]] - mixture[i][1]*embeddings[indices[i][1]] - mixture[i][2]*embeddings[indices[i][2]] - mixture[i][3]*embeddings[indices[i][3]] - mixture[i][4]*embeddings[indices[i][4]])**2, sequences=areasIndices )[0].sum()
+loss = theano.scan( lambda i: 100000*(target[i] - mixture[i][0]*embeddings[indices[i][0]] - mixture[i][1]*embeddings[indices[i][1]] - mixture[i][2]*embeddings[indices[i][2]] - mixture[i][3]*embeddings[indices[i][3]] - mixture[i][4]*embeddings[indices[i][4]])**2 +
+                                abs(mixture[i][1]) + 10*abs(mixture[i][2]) + 100*abs(mixture[i][3]) + 1000*abs(mixture[i][4]), sequences=areasIndices )[0].sum() + 0.001*(embeddings**2).sum()
 gradients = T.grad(loss, params)
-lr = theano.tensor.scalar(name='lr')
+lr = T.scalar(name='lr')
 lr = 0.001
 updates = OrderedDict((p, p - lr*g) for p, g in zip(params, gradients))
 train = theano.function(inputs=[], outputs=loss, updates=updates)
 
 for it in range(10):
-    print(train())
+    print(lr, train())
+    print(embeddings[areas[0]._russia].eval())
+    lr = lr*0.95
